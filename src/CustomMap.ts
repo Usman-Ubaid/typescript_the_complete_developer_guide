46e83a239b5b336d10f18c1263f3c4209fb5517c
Usman Ubaid <usmanubaid9@gmail.com>
2025-10-28 21:50:46 +0100

    add map class and marker

---
+++ src/CustomMap.ts
@@ -0,0 +1,32 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(elementId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(elementId)!, {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: user.location,
+      label: user.name,
+    });
+  }
+
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: company.location,
+      label: company.companyName,
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-// import { User } from "./User";
-// import { Company } from "./Company";
+import { User } from "./User";
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
-const mapElement = document.getElementById("map");
-if (mapElement) {
-  new google.maps.Map(mapElement, {
-    zoom: 1,
-    center: {
-      lat: 0,
-      lng: 0,
-    },
-  });
-}
+const randomUser = new User();
+const randomCompany = new Company();
+
+const customMap = new CustomMap("map");
+
+customMap.addUserMarker(randomUser);
+customMap.addCompanyMarker(randomCompany);
