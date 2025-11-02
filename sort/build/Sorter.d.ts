b64a0eadbcacaca7400468ba016d42a78806d592
Usman Ubaid <usmanubaid9@gmail.com>
2025-11-02 16:06:11 +0100

    separating the logic

---
+++ sort/build/Sorter.d.ts
@@ -0,0 +1,7 @@
+import { NumberCollection } from "./NumberCollection.js";
+export declare class Sorter {
+    collection: NumberCollection;
+    constructor(collection: NumberCollection);
+    sort(): void;
+}
+//# sourceMappingURL=Sorter.d.ts.map
--- sort/build/index.js
+++ sort/build/index.js
@@ -1,24 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Sorter {
-    collection;
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const temp = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = temp;
-                }
-            }
-        }
-    }
-}
-const sorter = new Sorter([10, 3, -5, 0]);
+import { Sorter } from "./Sorter.js";
+import { NumberCollection } from "./NumberCollection.js";
+const numbersCollection = new NumberCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection);
 //# sourceMappingURL=index.js.map
