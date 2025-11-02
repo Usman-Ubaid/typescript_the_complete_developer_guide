ad73164eeb8483252b365631ecde33b8e1d16631
Usman Ubaid <usmanubaid9@gmail.com>
2025-11-02 14:14:37 +0100

    implement sorting

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,8 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hi");
-const logging = () => {
-    console.log("somthin!");
-};
-logging();
+class Sorter {
+    collection;
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
 //# sourceMappingURL=index.js.map
