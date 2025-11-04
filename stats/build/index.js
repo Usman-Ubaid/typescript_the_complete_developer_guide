292e5fe40280b9207ed32a5b0e1bb03cad47783f
Usman Ubaid <usmanubaid9@gmail.com>
2025-11-04 16:49:40 +0100

    feat: read csv file

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,3 +1,12 @@
-console.log("hi");
-export {};
+import { readFileSync } from "fs";
+const fileRead = () => {
+    const readingFile = readFileSync("./football.csv", { encoding: "utf-8" });
+    const splitFile = readingFile.split("\n");
+    const splittingMore = splitFile.map((value) => value.split(","));
+    console.log(splittingMore);
+};
+const matches = readFileSync("./football.csv", { encoding: "utf-8" })
+    .split("\n")
+    .map((row) => row.split(","));
+fileRead();
 //# sourceMappingURL=index.js.map
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,13 @@
-console.log("hi");
+import { readFileSync } from "fs";
+
+const fileRead = () => {
+  const readingFile = readFileSync("./football.csv", { encoding: "utf-8" });
+  const splitFile = readingFile.split("\n");
+  const splittingMore = splitFile.map((value) => value.split(","));
+  console.log(splittingMore);
+};
+
+const matches = readFileSync("./football.csv", { encoding: "utf-8" })
+  .split("\n")
+  .map((row): string[] => row.split(","));
+fileRead();
