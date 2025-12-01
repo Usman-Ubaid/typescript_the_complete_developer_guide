f317c79c96e1f5cb3911f9028d1093f6a5c75f29
Usman Ubaid <usmanubaid9@gmail.com>
2025-12-01 18:42:39 +0100

    feat: storing event listeners

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,5 +2,7 @@ import { User } from "./models/User";
 
 const newUser = new User({ name: "Usman", age: 29 });
 newUser.set({ name: "hello", age: 221 });
-console.log(newUser.get("age"));
-console.log(newUser.get("name"));
+newUser.on("click", () => {});
+newUser.on("click", () => {});
+newUser.on("mouseover", () => {});
+console.log(newUser);
