1d47cccfe918bd9004b3a736be15fd4b6b04bb42
Usman Ubaid <usmanubaid9@gmail.com>
2025-12-01 18:50:26 +0100

    feat: trigger event callback

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,7 +2,13 @@ import { User } from "./models/User";
 
 const newUser = new User({ name: "Usman", age: 29 });
 newUser.set({ name: "hello", age: 221 });
-newUser.on("click", () => {});
-newUser.on("click", () => {});
-newUser.on("mouseover", () => {});
-console.log(newUser);
+newUser.on("click", () => {
+  console.log("clicked 1");
+});
+newUser.on("click", () => {
+  console.log("clicked 2");
+});
+newUser.on("mouseover", () => {
+  console.log("mouseover");
+});
+console.log(newUser.trigger("click"));
