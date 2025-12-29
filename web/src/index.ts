457a9d4254d55e5703527d73b437694b12ed19e0
Usman Ubaid <usmanubaid9@gmail.com>
2025-12-29 15:24:40 +0100

    feat: json-server & adding fetch functionality

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,14 +1,3 @@
-import { User } from "./models/User";
+import axios from "axios";
 
-const newUser = new User({ name: "Usman", age: 29 });
-newUser.set({ name: "hello", age: 221 });
-newUser.on("click", () => {
-  console.log("clicked 1");
-});
-newUser.on("click", () => {
-  console.log("clicked 2");
-});
-newUser.on("mouseover", () => {
-  console.log("mouseover");
-});
-console.log(newUser.trigger("click"));
+axios.get("http://localhost:3000/users/2");
